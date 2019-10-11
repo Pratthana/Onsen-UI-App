5490f1212e83f65dd127cd6ff84488c4eedbf405
Pratthana <38246189+Pratthana@users.noreply.github.com>
2019-10-12 00:51:37 +0700

    up

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAX_9ujel3jRKl2wwceehy3UI7LXWULg_s",
+    authDomain: "fooddeliveryapp-27a82.firebaseapp.com",
+    databaseURL: "https://fooddeliveryapp-27a82.firebaseio.com",
+    projectId: "fooddeliveryapp-27a82",
+    storageBucket: "fooddeliveryapp-27a82.appspot.com",
+    messagingSenderId: "350183637276",
+    appId: "1:350183637276:web:4b87b3dd5df5af17197ad9",
+    measurementId: "G-FWHM1K4BQ0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
