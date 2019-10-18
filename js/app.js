4ea86758fa0e7be090ac7345f538761bc96c9c33
Pratthana <38246189+Pratthana@users.noreply.github.com>
2019-10-18 11:48:28 +0700

    up

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ document.addEventListener('init', function (event) {
 
   if (page.id === 'index') {
     console.log("index");
+    
 
     $("#login").click(function () {
       console.log("m")
@@ -38,20 +39,16 @@ document.addEventListener('init', function (event) {
     $("#gbtn").click(function () {
       var provider = new firebase.auth.GoogleAuthProvider();
       firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
         content.load('foodcategory.html');
+
       }).catch(function(error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
         var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
-        // ...
+        
       });
     });
 
